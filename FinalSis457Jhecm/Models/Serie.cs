b31e2217d11ed51b991bcaa142aeb84687d40269
JhesseniaC <jhessenia60@gmail.com>
2022-09-03 00:07:14 -0400

    Subiendo repos

---
+++ FinalSis457Jhecm/Models/Serie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalSis457Jhecm.Models
+{
+    public partial class Serie
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Sinopsis { get; set; }
+        public string? Director { get; set; }
+        public DateTime? FechaEstreno { get; set; }
+        public bool? RegistroActivo { get; set; }
+    }
+}
---
+++ FinalSis457Jhecm/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FinalSis457Jhecm.Models
+{
+    public partial class Usuario
+    {
+        public int Id { get; set; }
+        public string? Usuario1 { get; set; }
+        public string? Clave { get; set; }
+        public string? Rol { get; set; }
+        public bool? RegistroActivo { get; set; }
+    }
+}
